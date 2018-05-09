434c7185100c4da6b48202bf40674748b19f34e3
craftsmon <craftsmon@outlook.com>
2018-05-09 15:58:22 +0300

    Lesson 1: Hello world example

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+const { app, BrowserWindow } = require('electron')
+
+let MainWindow;
+
+app.on('ready', () => {
+    MainWindow = new BrowserWindow()
+    MainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
+})
