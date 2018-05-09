b34a8a7a053e7a898461595570436deb57444df2
craftsmon <craftsmon@outlook.com>
2018-05-09 17:18:27 +0300

    Lesson 2: Storing user data across instances

--- Readme.md
+++ Readme.md
@@ -1,4 +1,6 @@
 ## Electron Lessons
 
-This is my journey towards learning electron app desktop environment.
-I really hope am gonna finish this one.
+### Lesson 2
+
+This lesson is about how to save application data and persist the data across restarts of the application.
+For this example we store the position of the app window, and after closing and starting again the window positions itself where it was the last time.
--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const path = require('path')
 
 const { app, BrowserWindow } = require('electron')
 
+const Store = require('./store')
+
 let MainWindow;
 
+const store = new Store({
+  configName: 'user-preferences',
+  defaults: {
+    windowBounds: {
+      height: 600,
+      width: 800,
+      x: 0, y: 0
+    }
+  }
+})
+
 app.on('ready', () => {
-    MainWindow = new BrowserWindow()
-    MainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
+  MainWindow = new BrowserWindow(store.get('windowBounds'))
+
+  function saveWindowBounds() {
+    store.set('windowBounds', MainWindow.getBounds())
+  }
+
+  // listen to `resize` and `move` and save settings
+  MainWindow.on('resize', saveWindowBounds)
+  MainWindow.on('move', saveWindowBounds)
+  
+  MainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
 })
