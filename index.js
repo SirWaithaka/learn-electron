0a20d930dae1136f87817b46221f332d1925e629
craftsmon <craftsmon@outlook.com>
2018-05-09 18:34:43 +0300

    Lesson 4: Interprocess communication; The  and  processes;  APIs on the

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const path = require('path')
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 
 let MainWindow;
 
 app.on('ready', () => {
   MainWindow = new BrowserWindow()  
   MainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
-  MainWindow.webContents.openDevTools()
+  
+  ipcMain.on('show-dialog', (e, { type }) => {
+    dialog.showMessageBox(MainWindow, {
+      type,
+      buttons: [],
+      message: 'Ghello, how are you?'
+    })
+  })
 })
