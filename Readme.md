6b4f4bf173ffb08edabc48459a8637e3fe780d51
craftsmon <craftsmon@outlook.com>
2018-05-11 18:14:20 +0300

    Lesson 6: learning about menus and submenus in electron

--- Readme.md
+++ Readme.md
@@ -1,10 +1,6 @@
 ## Electron Lessons
 
-### Lesson 5
+### Lesson 6
 
-#### Keytar
-A native Node module to get, add, replace, and delete passwords in system's keychain. On macOS the passwords are managed by the Keychain, on Linux they are managed by the Secret Service API/libsecret, and on Windows they are managed by Credential Vault.
-
-In this lesson we store sensitive data using system level libraries provided by the OS. To achieve this we use node module `[keytar v4.2.1](https://www.npmjs.com/package/keytar)`.
-
-All functions in this version of keytar node module are asynchronous and return a promise. Always remember to catch errors.
+#### Menus
+Just an introduction to Menus
--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const path = require('path')
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog, Menu } = require('electron')
 
 let MainWindow;
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('ready', () => {
   MainWindow = new BrowserWindow()  
   MainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
+
+  let application_menu = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'Ctrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Open',
+          accelerator: 'Ctrl+O',
+          click () {
+            dialog.showOpenDialog({ properties: ['openFile', 'openDirectories', 'multiSelections']})
+          }
+        }
+      ]
+    }
+  ]
+
+  menu = Menu.buildFromTemplate(application_menu)
+  Menu.setApplicationMenu(menu)
+
+  MainWindow.on('closed', () => {
+    MainWindow = null
+  })
 })
