b248997d6b42eb2d248c202fb393790bae6c8bf1
craftsmon <craftsmon@outlook.com>
2018-05-15 17:54:04 +0300

    Most of the heavy llifting is done

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< a80a021afc3bbfbe9d4dd16ba3019f20f4ce2747
 const path = require('path')
 const url = require('url')
 
@@ -45,6 +46,55 @@ function createWindow () {
     }
   ])
   Menu.setApplicationMenu(menu)
+=======
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
+
+const {app, BrowserWindow, dialog, Menu} = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+	// Create the browser window.
+	win = new BrowserWindow({width: 800, height: 600})
+
+	// and load the index.html of the app.
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'src/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+
+	// Open the DevTools.
+	win.webContents.openDevTools()
+
+	// Emitted when the window is closed.
+	win.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null
+	})
+
+	// Build Menu
+	let menuTemplate = {
+		label: 'File',
+		submenu: [
+			{
+				label: 'Sound Control',
+				accelerator: 'Ctrl+O',
+				click () {
+					openFolderDialog()
+				}
+			}
+		]
+	}
+	let menu = Menu.buildFromTemplate([menuTemplate])
+	Menu.setApplicationMenu(menu)
+>>>>>>> Most of the heavy llifting is done
 }
 
 app.on('ready', createWindow)
@@ -59,6 +109,18 @@ app.on('activate', () => {
   }
 })
 
+<<<<<<< a80a021afc3bbfbe9d4dd16ba3019f20f4ce2747
 ipc.on('update-notify-value', (e, arg) => {
   win.webContents.send('targetPriceVal', arg)
 })
+=======
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+function openFolderDialog () {
+	dialog.showOpenDialog(win, {
+		properties: ['openFile']
+	}, function (filePath) {
+		win.webContents.send('music-file-path', filePath[0])
+	})
+}
+>>>>>>> Most of the heavy llifting is done
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,8 +1,33 @@
+const {ipcRenderer} = require('electron')
+const {Howl} = require('howler')
 const Vue = require('vue/dist/vue')
+
+let song = null
 
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'hello'
+    musicSelected: false
+  },
+  computed: {
+    select: {
+      get () { return song ? true : false },
+      set (v) { return this.musicSelected = v }
+    },
+    song () {
+      return new Howl({
+        src: [song]
+      }) 
+    }
+  },
+  methods: {
+    playMusic () {
+      this.song.play()
+    }
   }
+})
+
+ipcRenderer.on('music-file-path', (e, arg) => {
+  song = arg
+  app.select = true
 })
